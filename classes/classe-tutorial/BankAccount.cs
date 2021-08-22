6ca79f135acdb4b296750e7e646eea822e4e7ded
Marlon Manhães <79061586+AmMorgen@users.noreply.github.com>
2021-08-22 19:52:01 -0300

    estudo sobre classes
    Tutorial sobre classes disponivel na documentação oficial da linguagem C#

---
+++ classes/classe-tutorial/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classe_tutorial
+{
+    class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+           
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "initial balance");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount<= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+}
